3aae9919b9739ad2895afc51ca6734c0fb90c08a
Ramakrishna <ramkrish182957@gmail.com>
2023-10-26 17:10:21 +0530

    TODO APP - Added Section,Edit Components

--- src/components/TodoForm/TodoForm.js
+++ src/components/TodoForm/TodoForm.js
@@ -1,17 +1,51 @@
 import { useEffect, useRef } from "react";
 import style from "./TodoForm.module.css";
 
-const TodoForm = ({ edit }) => {
+const TodoForm = ({
+  edit,
+  setAddTodoFormVisible,
+  value,
+  setValue,
+  onSubmit,
+}) => {
   const titleRef = useRef();
 
   useEffect(() => {
     titleRef.current.focus();
   }, []);
 
+  const inputHandler = (e) => {
+    setValue({ ...value, [e.target.name]: e.target.value });
+  };
+
+  const formHandler = (e) => {
+    e.preventDefault();
+    if (e.nativeEvent.submitter.name === "cancel") {
+      setAddTodoFormVisible(false);
+      return;
+    }
+    if (!value.title) {
+      console.log("Please enter value");
+
+      !value.title && titleRef.current.focus();
+      return;
+    }
+    console.log("updated");
+    onSubmit();
+    setAddTodoFormVisible(false);
+  };
+
   return (
-    <form className={style.form}>
+    <form className={style.form} onSubmit={formHandler}>
       <h3>{edit ? "Edit Todo" : "Add New Todo"}</h3>
-      <input type="text" name="title" placeholder="Title..." ref={titleRef} />
+      <input
+        onChange={inputHandler}
+        type="text"
+        name="title"
+        value={value.title}
+        placeholder="Title..."
+        ref={titleRef}
+      />
       <div className={style.btn}>
         <button className={style.cancel} name="cancel" type="submit">
           Cancel
--- src/components/TodoList/TodoList.js
+++ src/components/TodoList/TodoList.js
@@ -1,5 +1,8 @@
 import style from "./TodoList.module.css";
 import Todo from "../Todo/Todo";
+import Section from "../../components/Section/Section";
+import TodoEdit from "../TodoEdit/TodoEdit";
+import { useState } from "react";
 
 // Static Data
 const todoList = [
@@ -22,10 +25,22 @@ const todoList = [
 ];
 
 const TodoList = () => {
+  const [addTodoFormVisible, setAddTodoFormVisible] = useState();
+  const [editValue, setEditValue] = useState();
+
   return (
     <section className={`container ${style.todoList}`}>
       <div>
         <h2>Todos</h2>
+        {addTodoFormVisible && (
+          <Section>
+            <TodoEdit
+              setAddTodoFormVisible={setAddTodoFormVisible}
+              value={editValue}
+              setValue={setEditValue}
+            />
+          </Section>
+        )}
       </div>
       {todoList.length === 0 ? (
         <h2 className={style.empty}>{"No Todos Found"}</h2>
